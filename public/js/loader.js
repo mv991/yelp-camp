be6ef49a3df9211968b9247e5af2b1608b26820c
Mahak Vaid <mahakvaid1999@gmail.com>
2024-03-09 16:49:06 +0530

    Added a loader

---
+++ public/js/loader.js
@@ -0,0 +1,5 @@
+// JavaScript to hide the loader when the page is fully loaded
+window.addEventListener("load", function () {
+  const loader = document.querySelector(".loader-wrapper");
+  loader.style.display = "none";
+});

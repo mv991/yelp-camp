706bbae59cc7d38c66867704c89662bd3df87ca3
Mahak Vaid <mahakvaid1999@gmail.com>
2024-03-16 10:53:46 +0530

    added loader

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,10 +1,20 @@
 // JavaScript to hide the loader when the page is fully loaded
-document.onreadystatechange = function() {
-    if (document.readyState !== "complete") {
-        document.querySelector("body").style.visibility = "hidden";
-        document.querySelector("#loader").style.visibility = "visible";
-    } else {
-        document.querySelector("#loader").style.display = "none";
-        document.querySelector("body").style.visibility = "visible";
+async function isServiceUp() {
+    try {
+      const response = await fetch("https://yelp-camp1-4b2u.onrender.com/"); 
+      console.log(response);
+
+    } catch (error) {
+      console.error('Error checking service status:', error);
+      return false; // Service is down or unreachable
     }
-};
+  }
+$(document).ready( async () => { 
+   var isUp = await isServiceUp();
+   if (isUp){
+      $("#loader").fadeOut(1000);
+      $("#content").fadeIn(1000);
+   } else {
+      $("#loader").show();
+   }
+});

7e051e23d03e5841676b50694ed8921dd228a746
Mahak Vaid <mahakvaid1999@gmail.com>
2024-03-16 11:12:16 +0530

    commited

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,7 +1,7 @@
 // JavaScript to hide the loader when the page is fully loaded
 async function isServiceUp() {
     try {
-      const response = await fetch("https://yelp-camp1-4b2u.onrender.com/"); 
+      const response = await fetch("https://yelp-camp1-4b2u.onrender.com"); 
       console.log(response);
 
     } catch (error) {
